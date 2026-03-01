515535e89d816440a797ddf02116ed8ae26e5216
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-03-01 13:19:49 +0300

    index.html dosyasindaki rakamlarin girildigi input alanini assets klasoru icindeki js klasoru icinde calculator.js dosyasinda seciyoruz ve baslangic degeri atamasi yapiyoruz

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -3,3 +3,6 @@ const display = document.querySelector('.calculator-input');
 
 // Hesap Makinasinda Bulunan Butonlarin Hepsini Seciyoruz
 const keys = document.querySelector('.calculator-keys');
+
+// Hesap Makinasinda Girilen Rakamlarin Gorundugu Alana Baslangic Degeri Atamasi Yapiyoruz
+let displayValue = '0';
