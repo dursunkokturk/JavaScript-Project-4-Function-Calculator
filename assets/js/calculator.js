4f803c362d256b166fba0e274928bc73050fe50f
Dursun Kokturk <dursunserefkokturk@gmail.com>
2026-03-01 13:15:27 +0300

    index.html dosyasindaki tum butonlari assets klasoru icindeki js klasoru icinde calculator.js dosyasinda seciyoruz

---
+++ assets/js/calculator.js
@@ -0,0 +1,5 @@
+// Hesap Makinasinda Girilen Rakamlarin Gorundugu Alani Seciyoruz
+const display = document.querySelector('.calculator-input');
+
+// Hesap Makinasinda Bulunan Butonlarin Hepsini Seciyoruz
+const keys = document.querySelector('.calculator-keys');
